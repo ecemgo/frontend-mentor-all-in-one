cc742e3d2e64ed03e1df35c56d6a56b4a1a89f82
Ecem Gokdogan <ecemgo@hotmail.com>
2023-05-04 15:09:33 +0300

    added 'scroll to top' button

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+//! Hamburger menu
+
 const navbar = document.querySelector(".navbar-nav");
 const togglebtn = document.querySelector(".nav-toggle-btn");
 const navitem = document.querySelectorAll(".nav-item");
@@ -12,3 +14,26 @@ togglebtn.addEventListener("click", hamburgermenu);
 for (let i = 0; i < navitem.length; i++) {
   navitem[i].addEventListener("click", hamburgermenu);
 }
+
+//! Scroll to top
+
+const scrollBtn = document.querySelector(".btn");
+
+const btnVisibility = () => {
+  if (window.scrollY > 400) {
+    scrollBtn.style.visibility = "visible";
+  } else {
+    scrollBtn.style.visibility = "hidden";
+  }
+};
+
+document.addEventListener("scroll", () => {
+  btnVisibility();
+});
+
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
